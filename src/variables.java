799d959862c89ced1f564dceb54de159607b2dab
Shel Valdés <creative@shelvaldes.com>
2021-09-05 18:29:26 -0500

    some exercises

---
+++ src/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        int salary = 18000;
+        salary += 2000;
+
+        String employeeName = "Juan";
+        employeeName = employeeName + " Pérez";
+
+        System.out.println(employeeName + " gana $" + salary);
+    }
+}
