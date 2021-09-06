eca2887d6abaa4cdfe6572188fdb2fd402880de3
Shel Valdés <creative@shelvaldes.com>
2021-09-06 18:45:08 -0500

    string notes

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String[] diasSemana = new String[7];
+        String[][] ciudades = new String[4][2]; //altura por base. 4 renglones, 2 columnas
+
+        //some code notes
+        byte i = 1; byte j = 1; byte k = i+j;
+    }
+}
