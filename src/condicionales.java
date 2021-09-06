7e527fd07fc841594da36daf3b812598a469aa05
Shel Valdés <creative@shelvaldes.com>
2021-09-06 16:29:12 -0500

    Hackerrank library fine solved

--- src/condicionales.java
+++ src/condicionales.java
@@ -2,27 +2,36 @@ public class condicionales {
     public static void main(String[] args) {
         // los condicionales y el scope funcionan igual que en Javascript
         //sacado de una prueba de hackerrank
-        int d1 = 23;
-        int m1 = 12;
-        int y1 = 2022;
+        int d1 = 2;
+        int m1 = 7;
+        int y1 = 1014;
         //due
-        int d2 = 31;
-        int m2 = 12;
-        int y2 = 2021;
+        int d2 = 1;
+        int m2 = 1;
+        int y2 = 1015;
         //fine
         int fine;
 
-        if (y1 <= y2){
-            if (m1 <= m2){
+        //same year
+        if (y1 == y2){
+            //same month
+            if (m1 == m2){
                 if (d1 <= d2){
                     fine = 0;
                 }else{
-                    fine = 15 * (d1 + d2);
+                    fine = 15 * (d1 - d2);
                 }
-
-            } else {
+                //monts before
+            } else if (m1<m2){
+                fine = 0;
+                //past months
+            }else {
                 fine = 500 * (m1 - m2);
             }
+            //a year(s) before
+        }else if (y1 < y2){
+            fine = 0;
+            //past a year
         }else{
             fine = 10000;
         }
